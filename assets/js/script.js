9c5e56d8755307171519f2b72a2caaf44e253e05
Nico Van Der Walt <nvanderwalt12@yahoo.com>
2024-06-08 13:43:02 +0000

    Declare result with a conditional operator to call getResult (function) spesified parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
+console.log("Hi There!")
+
 let player1Score = 0;
 let player2Score = 0;
 let computerScore = 0;
 
 /*Function to play as single player
-Player1 will choose their option by using the onClick event*/
+Player1 will choose their option by using the onclick event*/
 function play(playerChoice, Multiplayer = false) {
     const choices = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
     let computerChoice = choices[Math.floor(Math.random() * choices.length)]; // Pick a random choice for the computer in the array of choices
     let player2Choice = null; //variable has been declared but hasn’t been assigned a value
 
     if (Multiplayer) {
-        player2Choice = prompt("Player 2, enter your choice (rock, paper, scissors, spock, lizard):").toLowerCase(); //Player2 will play with keyboard thus prompt input
+        player2Choice = prompt("Player 2, enter your choice (rock, paper, scissors, spock, lizard):").toLowerCase(); //Player2 will play with keyboard through prompt input
         while (!choices.includes(player2Choice)) {   //While loop continue until player2 have a valid choice
             alert("Your choice is NOT valid. Please try again.");
             player2Choice = prompt("Player 2, enter your choice (rock, paper, scissors, spock, lizard):").toLowerCase();
         }
     }
+}
